533d2203708d2ac022e8ce876281b814b7d7066d
cetc28 <2962106617@qq.com>
2022-05-20 14:16:37 +0800

    second change

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello{
      public static void main(String[] args){
-
-	system.out.println.("hello");
+	system.out.println.("hello git init");
+	system.out.println.("modify by cetc28");
 }
 }
